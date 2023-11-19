4c7625d3e67d502de502ec25d46cd297fc8f402e
Gopporo <sasapopov892@gmail.com>
2023-11-19 23:04:49 +0300

    1.3

--- task3.py
+++ task3.py
@@ -1,11 +1,40 @@
-def main():
-    subjects = {}
-    with open("Занятия.txt", "r", encoding="utf-8") as f:
-        for line in f:
-            subject, lectures, practices, labs = line.split()
-            subjects[subject] = int(lectures) + int(practices) + int(labs)
+subjects = {}
 
-    print(subjects)
 
-if __name__ == "__main__":
-    main()
+def count(str):
+    sum = 0
+    number = ""
+    c = 0
+    for i in range(len(str) - 1):
+        number = ""
+        if (str[c].isdigit()):
+            number += str[c]
+            cou = c + 1
+            while (str[cou].isdigit()):
+                number += str[cou]
+                cou += 1
+
+            sum += int(number)
+        if (len(number) == 0):
+            c += 1
+        else:
+            c += len(number)
+
+        if (c >= len(str)):
+            break
+        print(number)
+
+    return sum
+
+
+with open("Занятия.txt", encoding="utf-8") as f:
+    for line in f:
+        parts = line.split(":")
+
+        subject = parts[0].strip()
+
+        summa = count(parts[1])
+
+        subjects[subject] = summa
+
+print(subjects)
